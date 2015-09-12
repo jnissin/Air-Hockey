193ff254a099fd7b50e7ec980fa706f04fc07c86
Sten Hägglund <sten.hagglund@gmail.com>
2015-09-12 20:46:47 +0300

    Basic score GameMaster

---
+++ Assets/Scripts/GameMasterController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+/*
+1) Hold score
+2) Hold gametime
+3) Manage players (network: connect/disconnect, game creation, etc.)
+
+*/
+public class GameMasterController : MonoBehaviour {
+	
+	public ReactiveProperty<int> PlayerOneScore { get; protected set; }
+	public ReactiveProperty<int> PlayerTwoScore { get; protected set; }
+
+	// Use this for initialization
+	void Start () {
+		PlayerOneScore = new ReactiveProperty<int>(0);
+		PlayerTwoScore = new ReactiveProperty<int>(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
