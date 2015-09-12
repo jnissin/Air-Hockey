612b14b79456e2b54f552027dafedc7598884500
Sten Hägglund <sten.hagglund@gmail.com>
2015-09-12 21:53:16 +0300

    Add score displays to GameMaster

--- Assets/Scripts/GameMasterController.cs
+++ Assets/Scripts/GameMasterController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Collections;
 using UniRx;
@@ -9,18 +10,28 @@ using UniRx;
 */
 public class GameMasterController : MonoBehaviour {
 	
+	private int _startTime = 0;
+	
 	public IntReactiveProperty PlayerOneScore { get; protected set; }
 	public IntReactiveProperty PlayerTwoScore { get; protected set; }
 	public IntReactiveProperty GameTimeSeconds { get; protected set; }
+	
+	public Text PlayerOneScoreDisplay = null;
+	public Text PlayerTwoScoreDisplay = null;
+	
 
 	// Use this for initialization
 	void Start () {
 		PlayerOneScore = new IntReactiveProperty(0);
 		PlayerTwoScore = new IntReactiveProperty(0);
 		GameTimeSeconds = new IntReactiveProperty(60);
+		
 		Observable
 			.Interval(TimeSpan.FromSeconds(1))
-			.Subscribe(_ => GameTimeSeconds.Value = GameTimeSeconds.Value > 1 ? GameTimeSeconds.Value - 1 : 0);
+			.Subscribe(_ => GameTimeSeconds.Value += 1);
+			
+		PlayerOneScore.Subscribe( score => PlayerOneScoreDisplay.text = score.ToString());
+		PlayerTwoScore.Subscribe( score => PlayerTwoScoreDisplay.text = score.ToString());
 	}
 	
 	// Update is called once per frame
