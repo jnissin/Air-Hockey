4361fa6670ecad6dfd265544bdc31752828cb8a6
Sten Hägglund <sten.hagglund@gmail.com>
2015-09-12 22:42:32 +0300

    Allow players to make goals

--- Assets/Scripts/GameMasterController.cs
+++ Assets/Scripts/GameMasterController.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System;
 using System.Collections;
 using UniRx;
+using UniRx.Triggers;
 
 /*
 3) Manage players (network: connect/disconnect, game creation, etc.)
@@ -18,6 +19,9 @@ public class GameMasterController : MonoBehaviour {
 	
 	public Text PlayerOneScoreDisplay = null;
 	public Text PlayerTwoScoreDisplay = null;
+	public GameObject PlayerOneGoal = null;
+	public GameObject PlayerTwoGoal = null;
+	public GameObject PuckPrefab = null;
 	
 
 	// Use this for initialization
@@ -32,6 +36,29 @@ public class GameMasterController : MonoBehaviour {
 			
 		PlayerOneScore.Subscribe( score => PlayerOneScoreDisplay.text = score.ToString());
 		PlayerTwoScore.Subscribe( score => PlayerTwoScoreDisplay.text = score.ToString());
+		
+		PlayerOneGoal
+			.OnTriggerEnter2DAsObservable ()
+			.Where (collision => collision.gameObject.CompareTag("Puck"))
+			.Subscribe (_ => PlayerTwoScore.Value += 1);
+		
+		PlayerTwoGoal
+			.OnTriggerEnter2DAsObservable ()
+			.Where (collision => collision.gameObject.CompareTag("Puck"))
+			.Subscribe (_ => PlayerOneScore.Value += 1);
+
+		PlayerOneGoal
+			.OnTriggerExit2DAsObservable ()
+			.Merge (PlayerTwoGoal.OnTriggerExit2DAsObservable())
+			.Where (trigger => trigger.gameObject.CompareTag("Puck"))
+			.Subscribe(ev => { 
+				ev.gameObject
+		           .OnDestroyAsObservable ()
+		           .Delay (TimeSpan.FromSeconds (1))
+		           .Subscribe (_ => Instantiate (PuckPrefab));
+				Destroy(ev.gameObject);
+			});
+
 	}
 	
 	// Update is called once per frame
